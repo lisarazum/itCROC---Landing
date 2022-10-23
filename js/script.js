2cf3fccd61091e9f74412709376ac01bb6445212
lisarazum <liza0046@gmail.com>
2022-10-23 23:49:45 +0300

    transitions

--- js/script.js
+++ js/script.js
@@ -2,8 +2,12 @@
 
 const swiper = new Swiper('.clients__swiper', {
   loop: true,
+  slidesPerView: 4,
+  slidesPerGroup: 4,
+  spaceBetween: 10,
 
   pagination: {
     el: '.clients__swiper-pagination',
+    clickable: true,
   },
 });
