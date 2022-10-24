9c388a7090245dd414f34960fede99b32621580f
lisarazum <liza0046@gmail.com>
2022-10-24 17:36:14 +0300

    1024 done till work

--- js/script.js
+++ js/script.js
@@ -11,3 +11,59 @@ const swiper = new Swiper('.clients__swiper', {
     clickable: true,
   },
 });
+
+//accordion
+
+var acc = document.getElementsByClassName("steps__accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight){
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
+// validation
+
+const validation = new window.JustValidate('#consult-form');
+
+validation
+  .addField('#form-name', [
+    {
+      rule: 'minLength',
+      value: 3,
+      errorMessage: 'Ваше имя должно включать минимум 3 символа',
+    },
+    {
+      rule: 'maxLength',
+      value: 30,
+      errorMessage: 'Ваше имя должно включать максимум 30 символов',
+    },
+  ])
+  .addField('#form-email', [
+    {
+      rule: 'required',
+      errorMessage: 'Заполните поле',
+    },
+    {
+      rule: 'email',
+      errorMessage: 'Неправильный email',
+    },
+  ])
+  .addField('#form-tel', [
+    {
+      rule: 'required',
+      errorMessage: 'Заполните поле',
+    },
+    {
+      rule: 'string',
+      value: Number,
+      errorMessage: 'Неправильный телефон',
+    },
+  ]);
